c9f36e256796cdb9d0d4e027c2c45445618da479
PradeepPs04 <pradeep.ps2004@gmail.com>
2025-01-21 12:57:14 +0530

    styling my cart page for student

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+
 import { IconBtn } from '../../../common/IconBtn';
 
 export const RenderTotalAmount = () => {
@@ -12,9 +13,9 @@ export const RenderTotalAmount = () => {
     }
 
   return (
-    <div>
-        <p>Total:</p>
-        <p>Rs {total}</p>
+    <div className="min-w-[280px] rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6">
+        <p  className="mb-1 text-sm font-medium text-richblack-300">Total:</p>
+        <p className="mb-6 text-3xl font-medium text-yellow-100">Rs {total}</p>
 
         <IconBtn
             text="Buy Now"
